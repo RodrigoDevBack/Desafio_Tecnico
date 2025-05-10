b78301859c1247222a6f4ddf8907d94bdf3655b0
Rodrigo <155842654+RodrigoDevBack@users.noreply.github.com>
2025-05-10 15:14:34 -0300

    .

--- Back_End/Models/model.py
+++ Back_End/Models/model.py
@@ -4,7 +4,7 @@ from tortoise import fields
 class Project_Manager(Model):
     id = fields.IntField(primary_key = True)
     name = fields.CharField(max_length = 60)
-    description = fields.TextField(primary_key = True)
+    description = fields.TextField()
     status = fields.TextField()
     created_at = fields.DatetimeField(auto_now = True)
     def __str__(self):
